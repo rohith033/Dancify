26c8e78aff46663b7279040e4d26643b9ac32a01
rohith <102001026@smail.iitpkd.ac.in>
2023-01-02 13:00:13 +0530

    firstcommit

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => (
+  <div className="w-full flex justify-center items-center flex-col">
+  <h1 className="font-bold text-2xl text-white mt-2">
+    Something Went Wrong
+  </h1>
+</div>
+);
+
+export default Error;
